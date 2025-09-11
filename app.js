f50e2d50312d9fd1a012ea350b3e9b2858ac77b4
Muhammed Sudheer A.S <muhammedsudheer948@gmail.com>
2025-09-11 20:09:47 +0530

    To-Do App with localStorage

--- app.js
+++ app.js
@@ -2,18 +2,55 @@ const form = document.getElementById("todoForm");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+tasks.forEach(renderTask);
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const task = taskInput.value.trim();
-    if (!task) {
-        console.log("no task is there");
-        return;
-    };
+    if (!task) return;
+    
+    const newTask = { text: task, completed: false };
 
-    const li = document.createElement("li");
-    li.textContent = task;
+    tasks.push(newTask);
 
-    taskList.appendChild(li);
+    saveTasks();
+
+    renderTask(newTask);
     taskInput.value = "";
 });
+
+function renderTask(taskObj) {
+    const li = document.createElement("li");
+    
+    const span = document.createElement("span");
+    span.textContent = taskObj.text;
+
+    if (taskObj.completed) {
+        span.classList.add("completed");
+    }
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+
+    span.addEventListener("click", () => {
+        taskObj.completed = !taskObj.completed;
+        span.classList.toggle("completed");
+        saveTasks();
+    });
+
+    deleteBtn.addEventListener("click", () => {
+        tasks = tasks.filter( t => t !== taskObj);
+        li.remove();
+        saveTasks();
+    });
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+}
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
